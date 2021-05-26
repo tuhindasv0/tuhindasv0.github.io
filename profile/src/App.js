faeba5d2e317861fbb8675823a4fe8a8f987889e
Tuhin Dev <tuhindasv0@gmail.com>
2021-05-26 19:08:16 +0530

    adding new changes

--- profile/src/App.js
+++ profile/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactadadddddddddddd
+          I will start working
         </a>
       </header>
     </div>
