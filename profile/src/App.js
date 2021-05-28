12e58576a9fc27d7c2e439e853855ffbd3b73b33
Tuhin Dev <tuhindasv0@gmail.com>
2021-05-28 12:24:40 +0530

    removed unneccesary things and added profile pic

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import logo from './resources/img/profile.jpeg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I will start working
-        </a>
-      </header>
-    </div>
+   
+      <div className="profilePic">
+        <img src={logo} alt="Italian Trulli" height="200px" width="auto"></img>
+        </div>
+    
   );
 }
 
