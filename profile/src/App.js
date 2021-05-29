5bcc6edaf660d7e5b053807cf4c5df174238d07d
Tuhin Dev <tuhindasv0@gmail.com>
2021-05-30 00:35:48 +0530

    added few lines in about section

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,13 +1,30 @@
 import logo from './resources/img/profile.jpeg';
+import TextLoop from "react-text-loop";
+import Link from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
    
+    <div>
       <div className="profilePic">
         <img src={logo} alt="profile pic" height="200px" width="auto"></img>
         </div>
-    
+        <div className="profileHeader">
+
+        <h2>
+          I am <br></br>
+                        <TextLoop>
+                            <span> AWS </span>
+                            
+                            <span> Java </span>
+                            <span> React </span>
+                        </TextLoop>{" "}
+                        <br></br>
+                       Developer
+                    </h2>
+                </div>
+                </div>
   );
 }
 
