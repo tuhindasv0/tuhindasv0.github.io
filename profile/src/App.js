fada4536b79cba469a1151429d29a5c33a978dd3
Tuhin Dev <tuhindasv0@gmail.com>
2021-06-05 15:16:08 +0530

    changes

--- profile/src/App.js
+++ profile/src/App.js
@@ -1,30 +1,28 @@
-import logo from './resources/img/profile.jpeg';
+import logo from "./resources/img/profile.jpeg";
 import TextLoop from "react-text-loop";
 import Link from "react-router-dom";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   
     <div>
       <div className="profilePic">
         <img src={logo} alt="profile pic" height="200px" width="auto"></img>
-        </div>
-        <div className="profileHeader">
-
+      </div>
+      <div className="profileHeader">
         <h2>
           I am <br></br>
-                        <TextLoop>
-                            <span> AWS </span>
-                            
-                            <span> Java </span>
-                            <span> React </span>
-                        </TextLoop>{" "}
-                        <br></br>
-                       Developer
-                    </h2>
-                </div>
-                </div>
+          <TextLoop>
+            <span> AWS </span>
+
+            <span> Java</span>
+            <span> React </span>
+          </TextLoop>{" "}
+          <br></br>
+          Developer
+        </h2>
+      </div>
+    </div>
   );
 }
 
