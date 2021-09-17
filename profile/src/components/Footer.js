d244689b0884c3852cbcc28c2f00c455ee08a856
tuhindasv0 <tuhindasv0@gmail.com>
2021-09-17 18:04:42 +0530

    add github

--- profile/src/components/Footer.js
+++ profile/src/components/Footer.js
@@ -1,14 +1,24 @@
-var { SocialIcon } = require('react-social-icons');
 
+var { SocialIcon } = require("react-social-icons");
 function Footer() {
-  return <div className="Footer">Copyright &#169; Tuhin Das. All rights reserved 
-  
-  <SocialIcon url="https://linkedin.com/" />
-  <SocialIcon url="https://twitter.com/" />
-  <SocialIcon url="https://facebook.com/" />
-  <SocialIcon url="https://youtube.com/" />
-   </div>;
-  
+  return (
+    <div className="Footer">
+      <div className="ItemPlacement">
+        Copyright &#169; Tuhin Das. All rights reserved
+      </div>
+      <br></br>
+      <br></br>
+      <div className="LogoPlacement">
+        <SocialIcon url="https://linkedin.com/" />
+        <SocialIcon url="https://twitter.com/" />
+        <SocialIcon url="https://facebook.com/" />
+        <SocialIcon url="https://youtube.com/" />
+        <SocialIcon url="https://Github.com/tuhindasv0" />
+      
+        
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
